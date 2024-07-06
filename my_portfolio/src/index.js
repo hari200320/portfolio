63c867c9e91d19b4b69c6eb8f176f4785bb742d2
Hari <hariharanr200318@gmail.com>
2024-07-06 22:52:04 +0530

    portfolio

--- my_portfolio/src/index.js
+++ my_portfolio/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
+import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
